250c4779a6d6a1d071aaba0aeac3942051e81f99
15mikesmith <15mikesmith@gmail.com>
2018-07-26 20:30:22 -0400

    Can filter by video title

--- Assessment/static/main.js
+++ Assessment/static/main.js
@@ -72,11 +72,14 @@ $(document).ready(function(){
            });
 
 
-  //   $("#myInput").on("keyup", function() {
-  //   var value = $(this).val().toLowerCase();
-  //   $("h4").filter(function() {
-  //     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-  //   });
-  // });
+    $("#myInput").on("keyup", function() {
+
+    var value = $(this).val().toLowerCase();
+
+    $("h4").filter(function() {
+      $(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        //console.log(this.parent());
+    });
+  });
 
 });
