32ada8478b2ec1e4b0b6eb4ac86c2960e36b3bb9
15mikesmith <15mikesmith@gmail.com>
2018-07-26 20:03:50 -0400

    Added Comments and Navbar

--- Assessment/static/main.js
+++ Assessment/static/main.js
@@ -44,7 +44,6 @@ $(document).ready(function(){
            `)
            });
 
-
     }
 
     function mainVid(ID) {
--- Assessment/static/videoQuery.js
+++ Assessment/static/videoQuery.js
@@ -29,6 +29,8 @@ $(document).ready(function(){
     }
     var channel = $('section').attr('data-channel');
 
+    //Get videos from same author
+
 	    $.get(
     "https://www.googleapis.com/youtube/v3/search",{
       part : 'snippet',
@@ -40,5 +42,51 @@ console.log(data);
     resultsLoop(data)
       }
   );
+
+
+	        function commentsLoop(data){
+
+           $.each(data.items,function(i, item){
+
+           // var thumb = item.snippet.thumbnails.medium.url;
+           // var title = item.snippet.title;
+           // var desc = item.snippet.description.substring(0,100);
+           // var channel = item.snippet.channelId;
+           // var vid = item.id.videoId;
+
+               var comment = item.snippet.topLevelComment.snippet.textDisplay;
+               var name = item.snippet.topLevelComment.snippet.authorDisplayName;
+
+           $('#comments').append(`
+         <article class="comment">
+
+            <div class="content">
+                <h4>${name}</h4>
+                <p>${comment}</p>
+            </div>
+        </article>
+        
+           `)
+           });
+
+    }
+
+	    //Get Comments on Video
+    var idComments = $('section').attr('data-key');
+    var urlComments = "https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyDc5Y2QwI7t4r2xhEP8go6MrYccO9DreAo&textFormat=plainText&part=snippet&videoId="+ idComments
+
+	      $.ajax({
+        dataType: "jsonp",
+        type: 'GET',
+        url: urlComments,
+        success: function(result){
+            console.log(result);
+            // $('.data').text(data);
+            // console.log(data);
+            commentsLoop(result)
+    }});
+
+
+
 
 });
